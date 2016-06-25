f67381cefa157c6360238b62cc33c55c8b4864e3
Andrew Walker <subscriptions@awalker.ca>
2016-06-25 12:31:22 -0400

    Update index.js
    split values from labels

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 hexo.extend.tag.register('currency', function(args, content){
-  var foreign_currency = args[0];
-  var base_currency = args[1];
+  var foreign_currency_value = args[0];
+  var foreign_currency_symbol = args[1];
+  var base_currency_value = args[2];
+  var base_currency_symbol = args[3];
 
   var html = `<span class='currency'>
-                <span class='currency-foreign'>${foreign_currency}</span>
-                <span class='currency-base'>${base_currency}</span>
+                <span class='currency-foreign'>${foreign_currency_value} ${foreign_currency_symbol}</span>
+                <span class='currency-base'>${base_currency_value} ${base_currency_symbol}</span>
               </span>`;
   return html;
 });
