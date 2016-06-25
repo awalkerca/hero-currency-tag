a23e3d8c58c0a96ef6fc38714796fd1b04467338
Andrew Walker <subscriptions@awalker.ca>
2016-06-25 12:12:51 -0400

    Update index.js
    configure foreign and base currency

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 hexo.extend.tag.register('currency', function(args, content){
-  var foreign_currency;
-  var base_currency;
+  var foreign_currency = args[0];
+  var base_currency = args[1];
 
   var html = `<span class='currency'>
                 <span class='currency-foreign'>${foreign_currency}</span>
